58c42a24358fc2c2c8781eccfc17b81ef4b27bf4
Vivek Kumar <vivekgenius8287@gmail.com>
2024-02-16 11:23:02 +0530

    first commit

---
+++ calculator.py
@@ -0,0 +1,12 @@
+# Calculator
+
+a = int(input("Enter first number :"))
+b = int(input("Enter Second number :"))
+
+print(f"The Addition of {a} and {b} is :{a+b}")
+print(f"The Subtraction of {a} and {b} is :{a-b}")
+print(f"The Multiplication of {a} and {b} is :{a*b}")
+print(f"The Division of {a} and {b} is :{a/b}")
+print(f"The Modulus of {a} and {b} is :{a%b}")
+print(f"The Floor Division of {a} and {b} is :{a//b}")
+print(f"The Exponentiation of {a} and {b} is :{a**b}")
